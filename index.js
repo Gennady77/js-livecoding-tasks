c482dd0f549b5e0d72f763df91367b872b26bded
Gennady77 <gipper2006@gmail.com>
2022-11-08 13:57:34 +0300

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import './balanced_brackets';
 import './missing_number';
 import './sum_rotations';
+import './split_and_add';
---
+++ split_and_add.js
@@ -0,0 +1,21 @@
+function SplitAndAdd(arr, k) {
+  let n = arr.length;
+  let tmp = new Array(n * 2);
+  for (let i = 0; i < tmp.length; i++) {
+    tmp[i] = 0;
+  }
+
+  for (let i = 0; i < n; i++) {
+    tmp[i] = arr[i];
+    tmp[i + n] = arr[i];
+  }
+
+  for (let i = k; i < k + n; i++) {
+    arr[i - k] = tmp[i];
+  }
+}
+
+let arr = [12, 10, 5, 6, 52, 36];
+let n = arr.length;
+SplitAndAdd(arr, 2);
+console.log(arr); // [5, 6, 52, 36, 12, 10]
